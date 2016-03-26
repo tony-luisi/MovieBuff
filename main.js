5a28a9a38644a64d4483de4019a81c1a898a606c
tony-luisi <t.luisi@gmail.com>
2016-03-26 15:50:13 +1300

    added face and enter

--- main.js
+++ main.js
@@ -4,28 +4,31 @@ var request = require('superagent')
 $(document).ready(function() {
 
   //Tells jquery to wait until dom has loaded before loading jquery
-  var inputData = $("#button").click(function() {
+  $("#button").click(searchActor)
+  $("#searchInput").on("keydown",function(e) {
+    if (e.keyCode === 13){
+      searchActor()
+    }
+  })
 
+  function searchActor() {
     var rawInput = $("#searchInput").val().toString();
     getMovieByActor(rawInput, function(err, data){
-
-
       if (data.body.results.length > 0) {
         var movies = data.body.results[0].known_for
         renderMovieResults(movies)
-        console.log(data.body.results[0].id)
-        console.log(data.body.results[0])
         renderActor(data.body.results[0])
       } else {
+        $("#face").html("")
         $("#results").html("<br>Sorry, we could not find the actor you search for!")
       }
 
     })
-  })
+  }
 
   function renderMovieResults(movies) {
-    document.getElementById('results').innerHTML = ""
 
+    document.getElementById('results').innerHTML = ""
     //create outer div with an animation
     for (var i = 0; i < movies.length; i++) {
       var movieResult = document.createElement('div')
@@ -36,17 +39,12 @@ $(document).ready(function() {
   }
 
   function renderActor(actor) {
-
     document.getElementById('face').className = "animated fadeIn"
     document.getElementById('face').innerHTML = "<h3>" + actor.name + "</h3><br>" + "<img src='https://image.tmdb.org/t/p/w185/" + actor.profile_path +"'><br>"
-
-
   }
 
   function getMovieByActor(name, callback){
-
     name = escape(name)
-    console.log(name)
     request.get("http://api.themoviedb.org/3/search/person?api_key=da40aaeca884d8c9a9a4c088917c474c&query=" + name)
       .set('Accept', 'application/json')
       .end(function(err, res){
@@ -58,5 +56,4 @@ $(document).ready(function() {
     })
 
   }
-
 })
